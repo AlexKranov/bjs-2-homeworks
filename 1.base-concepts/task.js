1f6503a115d6d4a465db0523284db0bc7451ff9e
AlexKranov <86122490+AlexKranov@users.noreply.github.com>
2021-09-22 12:28:17 +0300

    update code

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,17 @@
+"use strict"
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+    let arr = [];
+    let d = b ** 2 - 4 * a * c;
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+    if (d < 0) {
+        arr.push();
+    } else if (d === 0) {
+        arr.push(-b / (2 * a));
+    } else {
+        arr.push((-b + Math.sqrt(d)) / (2 * a));
+        arr.push((-b - Math.sqrt(d)) / (2 * a));
+    }
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
+    return arr;
 }
