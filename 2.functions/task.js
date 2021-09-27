73c1df874b6e445cf0459ba74297d62d67635751
AlexKranov <86122490+AlexKranov@users.noreply.github.com>
2021-09-27 11:38:34 +0300

    new code

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,45 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min,max,sum,avg;
+    let min = Infinity,
+        max = -Infinity,
+        sum = 0,
+        avg;
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+        sum += arr[i];
+        avg = +(sum / arr.length).toFixed(2);
+    }
 
-  // Ваш код
-
-  return { min:min, max:max, avg:avg };
+    return { min: min, max: max, avg: avg };
 }
 
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
-  return sum;
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return +sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max
-
-  // Ваш кода
-  // for ...
-  
-  return max
+    let max = -Infinity;
+    for (let i = 0; i < arrOfArr.length; i++) {
+        let result = func(arrOfArr[i]);
+        if (max < result) {
+            max = result;
+        }
+    }
+    return max
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+    let difference = getArrayParams(arr);
+    return Math.abs(difference.max - difference.min);
 }
